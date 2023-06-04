30c66cfa891219231e84cb48e8d5376700eac9f8
Feldrise <admin@feldrise.com>
2023-06-04 10:25:38 +0200

    ✨ Basic PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {VitePWA, VitePWAOptions} from 'vite-plugin-pwa';
+
+const pwaManifest: Partial<VitePWAOptions> = {
+	registerType: 'autoUpdate',
+	manifest: {
+		name: 'Notifications PWA',
+		short_name: 'Notifications',
+		description: 'A simple PWA to test notifications',
+		display: 'standalone',
+		scope: '/',
+		start_url: '/',
+		orientation: 'portrait',
+	},
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		// eslint-disable-next-line new-cap
+		VitePWA(pwaManifest),
+	],
 });
